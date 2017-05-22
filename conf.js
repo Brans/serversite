cf0a643d03ece2364adff0ef4d0f5c6e1787658b
Brans <the.food@hotmail.com>
2017-05-22 23:03:15 +1000

    Add files via upload

---
+++ conf.js
@@ -0,0 +1,10 @@
+var servername = "ExampleServer";
+var plex = "32400/web"
+var sab = "8080";
+var tor = "9091";
+var tv = "8989";
+var hp = "8181";
+var mv = "8998";
+
+document.title = servername;
+document.getElementById( "heading" ).innerHTML = "<h1>" + servername + "</h1>";
